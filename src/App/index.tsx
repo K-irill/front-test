1fb47c9421f8eb838bb6aefb448727613c54a701
Kirill Kondrashkin <kirill.kondrashkin@inbox.ru>
2022-05-11 11:10:32 +0400

    add input

---
+++ src/App/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Input from '../components/Input';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='content'>
+      <header>
+        <Input/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+import './Input.scss'
+
+const Input: FC = () => {
+return (
+    // <input className="header-input" type="text" placeholder="Поиск" />
+    <img src="images/search-logo.svg" alt="sdas"/>
+)
+} 
+
+export default Input
